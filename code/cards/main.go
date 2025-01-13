060fdc22a2e3e5bad119f5c41624ab5174772cd8
Xu <whitenight.xiaxu@gmail.com>
2025-01-13 08:37:14 +0800

    Update main.go

--- code/cards/main.go
+++ code/cards/main.go
@@ -1,7 +1,18 @@
 package main
 
+import "fmt"
+
 func main() {
-	cards := newDeck()
-	cards.shuffle()
-	cards.print()
+	cards := []string{newCard(), newCard()}
+	cards = append(cards, "six of spades")
+
+	for i, card := range cards {
+		fmt.Println(i, card)
+	}
+
+	fmt.Println(cards)
+}
+
+func newCard() string {
+	return "five of diamonds"
 }
